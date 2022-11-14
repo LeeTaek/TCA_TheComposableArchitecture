bdfaed8a2975f7725a113166d900949f1136100d
leetaek <leetaek@openobject.net>
2022-11-14 15:09:25 +0900

    Initial Commit

---
+++ TheComposableArchitecture/TheComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheComposableArchitectureApp.swift
+//  TheComposableArchitecture
+//
+//  Created by healthwallet7 on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct TheComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
